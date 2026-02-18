7c509b545d16cbc36fed0f102efafc18f94e54f7
vrukshavalley <vrukshavalley@gmail.com>
2026-02-18 17:39:48 +0530

    Create page.tsx

---
+++ app/maintainence/page.tsx
@@ -0,0 +1,10 @@
+export default function Maintenance() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <div>
+        <h1>Page Temporarily Closed</h1>
+        <p>Our Balehonnur coffee shop is taking a short break. We'll be back soon!</p>
+      </div>
+    </div>
+  )
+}
