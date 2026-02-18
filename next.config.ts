41f350f5d93c36e9175589e0e207e460e787d743
vrukshavalley <vrukshavalley@gmail.com>
2026-02-18 17:40:55 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,24 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     qualities: [25, 50, 75, 95],
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'loremflickr.com',
-        pathname: '/**',
-      },
+      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'loremflickr.com', pathname: '/**' },
     ],
   },
-  
   async redirects() {
     return [
       {
-        source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-        destination: 'https://images.unsplash.com/photo-1586771107445-d3ca888129ff', // Temporary coffee image
+        source: '/((?!maintenance|_next/static|_next/image|favicon.ico).*)',
+        destination: '/maintenance',
         permanent: false,
       },
     ]
